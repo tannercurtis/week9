97bcad98e20a666f377d25484ba5319201dd121c
tannercurtis <tanner.curtis@nytimes.com>
2017-07-09 20:49:44 -0400

    Semi-complete

--- js/project.js
+++ js/project.js
@@ -4,10 +4,19 @@ $(".gallery").masonry({
   percentPosition: true
 });
 
+// $("data-fancybox").on("click", fancybox({}));
+
 $(".touchevents .thumbnail").hammer().bind("press", function() {
-// Your code here
+// // Your code here Only for devices with touch events, when the user presses on a thumbnail, 
+// show the caption by adding the class "open" to the thumbnail 
+// that fired the event.
+	$(this).addClass("open");
 });
 
 $(".touchevents .thumbnail").hammer().bind("swipe", function() {
-// Your code here
+// // Your code here: Only for devices with touch events, 
+// when the user swipes on a thumbnail, hide the caption by
+// removing the class "open" on the thumbnail that fired the event. 
+// Optionally, add an animation to the left position to animate the "swipe" effect.
+	$(this).removeClass("open");
 });
